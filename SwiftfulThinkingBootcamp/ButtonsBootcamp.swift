a7c41962cbb25618bf7d403767ac527e3d35b6f8
Shreya Pallan <shreyapallan@Shreyas-MBP.lan>
2024-09-05 15:42:00 +0530

    Buttons

--- SwiftfulThinkingBootcamp/ButtonsBootcamp.swift
+++ SwiftfulThinkingBootcamp/ButtonsBootcamp.swift
@@ -13,10 +13,66 @@ struct ButtonsBootcamp: View {
     
     var body: some View {
         VStack(spacing: 20) {
-            Text(title)
+            Text(title) // only string
+            
             Button("Press me") {
                 self.title = "Button was pressed"
             }
+            .accentColor(.red)
+            
+            Button(action: {
+                self.title = "Button #2 was pressed"
+            }, label: {
+                // can be any View
+                Text("Save".uppercased())
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .fontWeight(.semibold)
+                    .padding()
+                    .padding(.horizontal, 20)
+                    .background(
+                        Color.blue
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                    )
+                    
+                        
+            })
+            
+            Button {
+                self.title = "Button #3 was pressed"
+            } label: {
+                Circle()
+                    .fill(.white)
+                    .shadow(radius: 10)
+                    .frame(width: 75, height: 75)
+                    .overlay {
+                        Image(systemName: "heart.fill")
+                            .font(.title)
+                            .foregroundColor(
+                                .red
+                            )
+                    }
+            }
+            
+            Button {
+                self.title = "Button #4 was pressed"
+            } label: {
+                Text("Finish")
+                    .foregroundColor(.gray)
+                    .font(.caption)
+                    .bold()
+                    .padding()
+                    .padding(.horizontal, 20)
+                    .background(
+                        Capsule()
+                            .stroke(Color.gray, lineWidth: 2.0)
+                    )
+                  
+            }
+
+            
+           
         }
     }
 }
